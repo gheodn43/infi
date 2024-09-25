0c77f1f1faf03bf33766fb40eefcc6005de246f6
gheodn43 <97235672+gheodn43@users.noreply.github.com>
2024-09-25 16:10:02 +0700

    update main layout

--- src/components/layout/mainLayout.js
+++ src/components/layout/mainLayout.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Logo from '../buttons/logo';
-import AboutLink from '../buttons/AboutLink'; 
-import GuideLink from '../buttons/GuideLink'; 
+import AboutLink from '../buttons/AboutLink';
+import GuideLink from '../buttons/GuideLink';
 import LanguageSwitcher from '../buttons/LanguageSwitcher';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
@@ -9,17 +9,16 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 const mainLayout = (WrappedComponent) => {
     return (props) => {
         const [isMenuOpen, setIsMenuOpen] = useState(false);
-        const menuRef = useRef(null); // Create a ref for the menu
-        const toggleRef = useRef(null); // Create a ref for the toggle icon
+        const menuRef = useRef(null); 
+        const toggleRef = useRef(null);
 
         const toggleMenu = () => {
-            setIsMenuOpen(prev => !prev); // Toggle the menu state
+            setIsMenuOpen(prev => !prev); 
         };
 
         const handleClickOutside = (event) => {
-            // Check if the click is outside the menu and the toggle icon
             if (menuRef.current && !menuRef.current.contains(event.target) && !toggleRef.current.contains(event.target)) {
-                setIsMenuOpen(false); // Close the menu if clicking outside
+                setIsMenuOpen(false);
             }
         };
 
@@ -33,7 +32,7 @@ const mainLayout = (WrappedComponent) => {
         useEffect(() => {
             const handleResize = () => {
                 if (window.innerWidth >= 576) {
-                    setIsMenuOpen(false); // Close the menu on resizing to larger screens
+                    setIsMenuOpen(false);
                 }
             };
             window.addEventListener('resize', handleResize);
@@ -47,40 +46,38 @@ const mainLayout = (WrappedComponent) => {
                 <header className="bg-dark fixed-top">
                     <div className="container d-flex justify-content-between align-items-center h-custom">
                         <Logo />
-                        {/* Menu for small screens */}
-                        <div className="d-block d-sm-none" ref={toggleRef}>
-                            <FontAwesomeIcon 
-                                icon={faBars} 
-                                onClick={toggleMenu} 
-                                className="text-white cursor-pointer icon-size mb-0" 
+                        <div className="d-flex d-sm-none align-items-center" ref={toggleRef}>
+                            <LanguageSwitcher />
+                            <FontAwesomeIcon
+                                icon={faBars}
+                                onClick={toggleMenu}
+                                className="text-white cursor-pointer icon-size mb-0 ml-2"
                             />
                         </div>
-                        {/* Menu for larger screens */}
+
                         <div className="d-none d-sm-flex">
                             <AboutLink />
                             <GuideLink />
                             <LanguageSwitcher />
                         </div>
                     </div>
-                    {/* Display menu when clicking on faBars */}
                     {isMenuOpen && (
-                        <div 
-                            className="bg-dark text-white p-3 position-fixed" 
-                            style={{ right: 0, top: '56px', width: '50%' }} 
-                            ref={menuRef} // Attach the ref here
+                        <div
+                            className="bg-dark text-white p-3 position-fixed"
+                            style={{ right: 0, top: '56px', width: '50%' }}
+                            ref={menuRef}
                         >
                             <div className="d-flex flex-column align-items-start">
                                 <AboutLink className="mb-2" />
                                 <GuideLink className="mb-2" />
-                                <LanguageSwitcher />
                             </div>
                         </div>
                     )}
                 </header>
-                <main className="flex-fill mt-5 pt-5 px-2 px-md-3 px-lg-4 px-xl-5">
+                <main className="flex-fill mt-5 pt-5 px-2 px-md-3 px-lg-4 px-xl-5 min-vh-100">
                     <WrappedComponent {...props} />
                 </main>
-                <footer className="bg-dark fixed-bottom">
+                <footer className="bg-dark">
                     <p className="text-center text-white py-3">Footer</p>
                 </footer>
             </div>
