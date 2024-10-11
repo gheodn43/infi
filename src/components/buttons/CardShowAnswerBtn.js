e44fceceb9590e168275a9457ce388b0142c2245
gheodn43 <97235672+gheodn43@users.noreply.github.com>
2024-10-11 12:16:45 +0700

    u

--- src/components/buttons/CardShowAnswerBtn.js
+++ src/components/buttons/CardShowAnswerBtn.js
@@ -1,6 +1,6 @@
 export default function CardShowAnswerBtn({onStopCounter}) {
     return (
-        <div className="w-100 position-fixed bottom-0 left-0 justify-content-center px-5 py-3">
+        <div className="w-100 fixed-bottom d-flex left-0  px-5 py-3">
             <div className="mx-auto py-2 rounded-5 bg-light text-dark cursor-pointer w-md-25 text-center text-reponsive" onClick={onStopCounter}>Show Answer</div>
         </div>
     )
